fdc2a3a04abbde1cb6211b4409e3b633bbad35b5
MusaabAlfalahi <alfalahimusaab@gmail.com>
2024-06-14 21:39:34 +0300

    fix not null issue in available power_banks route

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,6 +1,7 @@
 class StationsController < ApplicationController
-  before_action :authorize_user, only: %i[index show create update destroy assign_to_location assign_to_warehouse]
+  before_action :authorize_user, only: %i[show create update destroy assign_to_location assign_to_warehouse]
   before_action :set_station, only: %i[show update destroy assign_to_location assign_to_warehouse]
+  before_action :authenticate_user, only: :index
 
   def index
     @stations = Station.all
